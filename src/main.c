e6ba8edf212b1cddd2506d7f3bbe0b952d33e4af
Ibrahim778 <m.ibrahim0798@gmail.com>
2021-07-19 14:48:25 +0500

    Done it properly now

--- src/main.c
+++ src/main.c
@@ -1,24 +1,35 @@
 #include <vitasdk.h>
+#include <taihen.h>
+#define printf sceClibPrintf
 
-/*    sceShellUtilInitEvents(0);
-    sceShellUtilLock(SCE_SHELL_UTIL_LOCK_TYPE_QUICK_MENU |
-                    SCE_SHELL_UTIL_LOCK_TYPE_POWEROFF_MENU |
-                    SCE_SHELL_UTIL_LOCK_TYPE_USB_CONNECTION |
-                    SCE_SHELL_UTIL_LOCK_TYPE_MC_INSERTED |
-                    SCE_SHELL_UTIL_LOCK_TYPE_MC_REMOVED |
-                    SCE_SHELL_UTIL_LOCK_TYPE_MUSIC_PLAYER |
-                    SCE_SHELL_UTIL_LOCK_TYPE_PS_BTN_2);*/
+
+SceUID hook;
+tai_hook_ref_t ref;
+
+SceUInt32 FUN_811c9bfa_Patch(void)
+{
+    //No need to display dialog (Pretend we've shown it before and it connected successfully)
+    return 1;
+}
 
 int DisableThread(SceSize args, void *argp)
 {
-    sceKernelDelayThread(4 * 1000000);
-    sceShellUtilLock(SCE_SHELL_UTIL_LOCK_TYPE_USB_CONNECTION);
+    sceKernelDelayThread(5 * 1000000);
+
+    tai_module_info_t modinfo;
+    modinfo.size = sizeof(modinfo);
+    taiGetModuleInfo("SceShell", &modinfo);
+
+    hook = taiHookFunctionOffset(&ref, modinfo.modid, 0, 0x1c9bfa, 1, FUN_811c9bfa_Patch);    
+
     return sceKernelExitDeleteThread(0);
 }
+
 
 void _start() __attribute__((weak, alias("module_start")));
 int module_start(SceSize argc, const void *args)
 {
+    printf("USBDisable by Ibrahim\n");
     int thread = sceKernelCreateThread("usbDisable", DisableThread, 191, 0x1000, 0, 0, NULL);
     sceKernelStartThread(thread, 0, NULL);
     return SCE_KERNEL_START_SUCCESS;
@@ -26,6 +37,8 @@ int module_start(SceSize argc, const void *args)
 
 int module_stop(SceSize argc, const void *args)
 {
-    sceShellUtilUnlock(SCE_SHELL_UTIL_LOCK_TYPE_USB_CONNECTION);
+    if(hook >= 0)
+        taiHookRelease(hook, ref);
+
     return SCE_KERNEL_STOP_SUCCESS;
 }
