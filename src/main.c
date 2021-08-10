4f172cd11755df20b615e68730fb26ace7495e0d
Electric1447 <itai.levin4@gmail.com>
2021-08-10 20:42:48 +0300

    Add support for fw 3.65+

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 SceUID hook;
 tai_hook_ref_t ref;
 
-SceUInt32 FUN_811c9bfa_Patch(void)
+SceUInt32 USBDisable_Patch(void)
 {
     //No need to display dialog (Pretend we've shown it before and it connected successfully)
     return 1;
@@ -18,9 +18,27 @@ int DisableThread(SceSize args, void *argp)
 
     tai_module_info_t modinfo;
     modinfo.size = sizeof(modinfo);
-    taiGetModuleInfo("SceShell", &modinfo);
 
-    hook = taiHookFunctionOffset(&ref, modinfo.modid, 0, 0x1c9bfa, 1, FUN_811c9bfa_Patch);    
+    if(taiGetModuleInfo("SceShell", &modinfo) >= 0)
+    {
+        switch (modinfo.module_nid)
+        {
+            case 0x0552F692: // 3.60 retail
+                hook = taiHookFunctionOffset(&ref, modinfo.modid, 0, 0x1c9bfa, 1, USBDisable_Patch); 
+                break;
+
+            case 0x5549BF1F: // 3.65 retail
+            case 0x34B4D82E: // 3.67 retail
+            case 0x12DAC0F3: // 3.68 retail
+            case 0x0703C828: // 3.69 retail
+            case 0x2053B5A5: // 3.70 retail
+            case 0xF476E785: // 3.71 retail
+            case 0x939FFBE9: // 3.72 retail
+            case 0x734D476A: // 3.73 retail
+                hook = taiHookFunctionOffset(&ref, modinfo.modid, 0, 0x1c9cc2, 1, USBDisable_Patch); 
+                break;
+        }
+    }
 
     return sceKernelExitDeleteThread(0);
 }
